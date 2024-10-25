167b09ee778301b41462b1188510a722c1e4738d
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-25 12:03:14 -0400

    Exercices from TypeScript title'Soma Simples'

---
+++ TypeScript/SomaSimples.js
@@ -0,0 +1,4 @@
+var numberA = 0;
+var numberB = 0;
+var soma = numberA + numberB;
+console.log("SOMA =" + " " + soma);
---
+++ TypeScript/SomaSimples.ts
@@ -0,0 +1,8 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n'); 
+
+let numberA:number = -30
+let numberB:number = 10
+let soma:number = numberA + numberB
+
+console.log("SOMA =" + " " + soma);
